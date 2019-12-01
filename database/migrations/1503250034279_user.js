faaeeb2cda7506447910177ed343d719e9a9a7dd
Renato Silveira De Lima <43325147+RenatoDKS@users.noreply.github.com>
2019-12-01 01:16:08 -0300

    Update 1503250034279_user.js

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -5,7 +5,7 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('user', (table) => {
+    this.create('users', (table) => {
       table.increments()
       table.string('name', 80).notNullable()
       table.string('lastName', 80).notNullable()
